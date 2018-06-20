07f0c57798f5c3e038f54716769909d289fcd7b0
wenweiliang <wliang@genome.wustl.edu>
2018-06-20 21:42:21 +0000

    Update README

--- MSIsensorWGS/generate_command.msisensor.sh
+++ MSIsensorWGS/generate_command.msisensor.sh
@@ -1,6 +1,6 @@
 ####
 #Aim: Generate google cloud command
-#Usage: bash generate_command.sh ${List_of_BAMs} ${JobName} ${DiskSize(in gb)}
+#Usage: bash generate_command.sh ${List_of_BAMs} ${JobName}
 #1. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.HighPass.txt HighPass 
 #2. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.LowPass.txt LowPass
 #Author: Wen-Wen Liang @ Wash U (liang.w@wustl.edu)
@@ -16,6 +16,7 @@ while read lines; do
 	tbam=$(echo ${tbampath} | awk -F "/" '{print $10}')
 	tumorid=$(echo $lines | awk -F " " '{print $2}')
 	size=$(echo $lines | awk -F " " '{print ($8*1.1)/1000000000}' | awk '{printf "%.0f", $1}')
+	#Disk size in GB
 	yaml="../msisensor.yaml"
 
 	echo "gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs normalbam=${nbampath},normalbai=${nbampath}.bai,tumorbam=${tbampath},tumorbai=${tbampath}.bai,id=${tumorid},msilist=gs://dinglab/wliang_msi/microsatellites.list,nbam=${nbam},tbam=${tbam} --outputs outputPath=gs://dinglab/wliang_msi/output/${tumorid}/${passvalue}/ --logging gs://dinglab/wliang_msi/logging/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.msisensor.sh
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# PancanFusionValidation
+# GoogleCloudWorks
+
+Various jobs have been run on google cloud.
+
+### PancanFusionValidation
 
 Scripts to run WGS validation pipeline for Pancan Fusion Project.
+
+`generate_yaml.*.sh`: Create site-specific yaml files for each gene fusion.
+
+`*.coordinate*.txt`: Coordinate for fusions. Input of `generate_yaml.*.sh`.
+
+`command.*.sh`: Commend for launching Google Pipeline API.
+
+`samtools_fusion.qc.yaml`: Template yaml for jobs.
+
+### MSIsensorWGS
+
+`TCGA.WGS.msisensor*.txt`: Result of msisensor run on TCGA WGS BAMs.
+
+`generate_command.msisensor.sh`: Launch jobs by using Google Pipeline API.
+
+`msisensor.yaml`: yaml file for running msisensor
+
+`worklog`: Get msisensor result from google cloud storage.
