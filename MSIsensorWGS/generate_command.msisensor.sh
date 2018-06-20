641023e0437e24e4f4e8899c3467b00f77683165
wenweiliang <wliang@genome.wustl.edu>
2018-06-20 21:23:51 +0000

    Clean folder structure

---
+++ MSIsensorWGS/generate_command.msisensor.sh
@@ -0,0 +1,22 @@
+####
+#Aim: Generate google cloud command
+#Usage: bash generate_command.sh ${List_of_BAMs} ${JobName} ${DiskSize(in gb)}
+#1. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.HighPass.txt HighPass 
+#2. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.LowPass.txt LowPass
+#Author: Wen-Wen Liang @ Wash U (liang.w@wustl.edu)
+####
+
+mkdir -p cloud
+echo "#" > cloud/gcloud_command.$2.msisensor.sh
+while read lines; do
+	passvalue=$2
+	nbampath=$(echo $lines | awk -F " " '{print $5}')
+	nbam=$(echo ${nbampath} | awk -F "/" '{print $10}')
+	tbampath=$(echo $lines | awk -F " " '{print $4}')
+	tbam=$(echo ${tbampath} | awk -F "/" '{print $10}')
+	tumorid=$(echo $lines | awk -F " " '{print $2}')
+	size=$(echo $lines | awk -F " " '{print ($8*1.1)/1000000000}' | awk '{printf "%.0f", $1}')
+	yaml="../msisensor.yaml"
+
+	echo "gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs normalbam=${nbampath},normalbai=${nbampath}.bai,tumorbam=${tbampath},tumorbai=${tbampath}.bai,id=${tumorid},msilist=gs://dinglab/wliang_msi/microsatellites.list,nbam=${nbam},tbam=${tbam} --outputs outputPath=gs://dinglab/wliang_msi/output/${tumorid}/${passvalue}/ --logging gs://dinglab/wliang_msi/logging/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.msisensor.sh
+done < $1
